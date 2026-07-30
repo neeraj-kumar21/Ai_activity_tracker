932b408654436f6e1716979f3d24bb49511060fa
Neeraj Kumar <neerajgound426@gmail.com>
2026-07-30 23:12:55 +0530

    add function to save data

---
+++ database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect("data/activity_tracker.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS activity_log
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  window_title TEXT)''')
+    
+    conn.commit()
+    conn.close()
+
+def save_activity(timestamp, window_title):
+
+    conn = sqlite3.connect("data/activity_tracker.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute('''INSERT INTO activity_log (timestamp, window_title)
+                      VALUES (?, ?)''', (timestamp, window_title))
+    
+    conn.commit()
+    conn.close()
+    
