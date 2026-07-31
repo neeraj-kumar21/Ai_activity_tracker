de873f0e65c22766904824f79806b8f1a215dbce
Neeraj Kumar <neerajgound426@gmail.com>
2026-07-31 20:23:11 +0530

    Window tracking task

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from activity_tracker import get_active_window_
+from activity_tracker import get_active_window_, get_active_window_
+from database import create_database, save_activity
+
+from datetime import datetime
 import time
 
+create_database()
+
+last_window = ""
+
 print("AI Activity Tracker is running...")
-print("-" * 40)
+
 
 while True:
-    active_window = get_active_window_()
 
-    print("Current active window:", active_window)
+    current_window = get_active_window_()
 
-    time.sleep(2)  # Check every 2 secondscl
+    if current_window != last_window:
+
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(current_time , current_window)
+
+    # Save the activity log 
+    save_activity(current_time, current_window)
+    
+    save_activity(current_time, current_window)
+
+    last_window = current_window
+
+    time.sleep(2)  # Check every 2 seconds
+
+def get_active_window():
+    ...
